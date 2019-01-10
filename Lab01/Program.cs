7a104eac918f50e0e059328d459eb0dee2cfeb28
Jason Few <omence11@gmail.com>
2019-01-09 18:44:05 -0800

    fixed exception handling

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -8,7 +8,7 @@ namespace Lab01
         //game prompt and question method calls
         static void Main(string[] args)
         {
-            
+
             Console.WriteLine("Lets play a guessing game about me.");
             QuestionOne();
             QuestionTwo();
@@ -23,17 +23,22 @@ namespace Lab01
             string incorrect = "No I was born in CA, press ENTER for next question";
             Console.WriteLine("What state was I born in CA, FL or WA?");
             string answerOne = Console.ReadLine().ToUpper();
+            
+            if (answerOne != "CA" && answerOne != "FL" && answerOne != "WA")
+            {
+                Console.WriteLine("Please answer with CA, FL or WA");
+                QuestionOne();
+                return " ";
+            }
 
-                if (answerOne == "CA")
-                {
-                    totalCorrect++;
-                    Console.WriteLine(correct);
-                    Console.ReadLine();
-                    return correct;
-                }
-                else Console.WriteLine(incorrect);
+            else if (answerOne == "CA")
+            {
+                totalCorrect++;
+                Console.WriteLine(correct);
                 Console.ReadLine();
-                return incorrect;
+                return "correct";
+            }
+            return "";
         }
 
 
@@ -67,36 +72,50 @@ namespace Lab01
 
         public static bool QuestionThree()
         {
-            Console.WriteLine("I have 3 dogs, True or False?");
-            string answerThree = Console.ReadLine().ToLower();
-            bool ansThree = Convert.ToBoolean(answerThree);
-            if (ansThree == true)
+            try
+            {
+                Console.WriteLine("I have 3 dogs, True or False?");
+                string answerThree = Console.ReadLine().ToLower();
+                bool ansThree = Convert.ToBoolean(answerThree);
+                if (ansThree == true)
+                {
+                    totalCorrect++;
+                    Console.WriteLine("Yes you are correct! Press ENTER for next question");
+                    Console.ReadLine();
+                    return ansThree;
+                }
+                else Console.WriteLine("No it is true I do have 3 dogs, press ENTER for next question");
+                Console.ReadLine();
+                return ansThree;
+
+            }
+            catch
+            {
+                Console.WriteLine("please answer True or False");
+                QuestionThree();
+                return true;
+            }
+        }
+
+        public static void QuestionFour()
+        { 
+
+            Console.WriteLine("I have 5 kids, True or False?");
+            string answerFour = Console.ReadLine().ToLower();
+            //bool ansFour = Convert.ToBoolean(answerFour);
+            if (answerFour != "true" && answerFour != "false")
+            {
+                Console.WriteLine("Please answer True or False");
+                QuestionFour();
+            }
+            else if (answerFour == "false")
             {
                 totalCorrect++;
                 Console.WriteLine("Yes you are correct! Press ENTER for next question");
                 Console.ReadLine();
-                return ansThree;
             }
-            else Console.WriteLine("No it is true I do have 3 dogs, press ENTER for next question");
-            Console.ReadLine();
-            return ansThree;
-
-
-        }
-
-        public static void QuestionFour()
-        {
-            Console.WriteLine("I have 5 kids, True or False?");
-            string answerFour = Console.ReadLine().ToLower();
-            bool ansFour = Convert.ToBoolean(answerFour);
-            if (ansFour == false)
-            {
-                totalCorrect++;
-                Console.WriteLine("Yes you are correct! Press ENTER for next question");
-                //Console.ReadLine();
-            }
-            else Console.WriteLine("No I actually have 2 kids, press ENTER for next question");
-            Console.ReadLine();
+            //else Console.WriteLine("No I actually have 2 kids, press ENTER for next question");
+            //Console.ReadLine();
         }
 
         public static int QuestionFive()
